1db7f8be6a3bdeb874ab6346e1d64f6b35112da6
Ch4m0rr0 <x34alejand36x@gmail.com>
2024-09-22 02:06:05 -0600

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,52 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { LoginComponent } from "./components/login/login.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ChatbotComponent } from './components/chatbot/chatbot.component';
 
 const routes: Routes = [
     {
-        path: '', redirectTo: '/register', pathMatch: 'full' 
-    },
-    {
-        path: 'register', component: RegisterComponent 
-    },
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+  {
+    path: '',
+    redirectTo: '/register',
+    pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
 
-    {
-        path: '', redirectTo: '/login', pathMatch: 'full' 
-    },
-    {
-        path: 'login', component: LoginComponent 
-    },
-    
-    {
-        path: 'home', component: HomeComponent 
-    }
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/chatbot',
+    pathMatch: 'full',
+  },
+  {
+    path: 'chatbot',
+    component: ChatbotComponent,
+  },
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes)
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/components/chatbot/chatbot.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbot',
+  standalone: true,
+  imports: [],
+  templateUrl: './chatbot.component.html',
+  styleUrl: './chatbot.component.css'
+})
+export class ChatbotComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
