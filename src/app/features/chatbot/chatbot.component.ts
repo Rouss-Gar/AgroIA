f39c1d37a89d5177f9fa89c98025abb4a615893d
RoussContollo <rosmery.garcia@contollo.net>
2024-10-18 15:00:03 -0600

    edit chatbot

--- src/app/features/chatbot/chatbot.component.ts
+++ src/app/features/chatbot/chatbot.component.ts
@@ -8,28 +8,86 @@ import { WitAiService } from '@/src/service/wit.service';
 export class ChatbotComponent {
   question: string = '';
   response: string = '';
+  recognition: any;
+  messages: { text: string; sender: string; time: string }[] = [];
 
-  constructor(private witAiService: WitAiService) {}
 
+  constructor(private witAiService: WitAiService) {
+    const { webkitSpeechRecognition }: any = window;
+    this.recognition = new webkitSpeechRecognition();
+    this.recognition.lang = 'es-ES';
+    this.recognition.continuous = false;
+    this.recognition.iterimResults = false;
+
+    this.recognition.onresult = (event: any) => {
+      this.question = event.results[0][0].transcript;  // Convierte la voz a texto
+      console.log('Texto detectado por voz:', this.question);
+      this.handleSubmit();  // Enviar la pregunta obtenida por voz
+    };
+    this.recognition.onerror = (event: any) => {
+      console.error('Error en el reconocimiento de voz:', event.error);
+    };
+  }
+
+  startVoiceRecognition() {
+    this.recognition.start();
+  }
+
+  // Método para enviar mensajes y recibir respuestas del bot
   async handleSubmit() {
-    try {
-      const data = await this.witAiService.getWitAiResponse(this.question);
-      const { entities } = data;
+    if (this.question.trim()) {
+      // Agregar el mensaje del usuario al array de mensajes
+      const userMessage = {
+        text: this.question,
+        sender: 'user',
+        time: this.getTime()
+      };
+      this.messages.push(userMessage);
 
-      if (entities && Object.keys(entities).length > 0) {        
-        const values = Object.keys(entities).flatMap((key) =>
-          entities[key].map((entity: any) => entity.value)
-        );
+      try {
+        
+        const data = await this.witAiService.getWitAiResponse(this.question);
+        const { entities } = data;
 
-        this.response = values.length > 0 ? values.join(', ') : 'No se encontraron valores en las entidades.';
-      } else {
-        this.response = 'No se encontraron entidades.';
+        let botResponse = '';
+
+        if (entities && Object.keys(entities).length > 0) {        
+          const values = Object.keys(entities).flatMap((key) =>
+            entities[key].map((entity: any) => entity.value)
+          );
+
+          botResponse = values.length > 0 ? values.join(', ') : 'No se encontraron valores en las entidades.';
+        } else {
+          botResponse = 'No se encontraron entidades.';
+        }
+
+        // Agregar la respuesta del bot al array de mensajes
+        const botMessage = {
+          text: botResponse,
+          sender: 'bot',
+          time: this.getTime()
+        };
+        this.messages.push(botMessage);
+
+        // Limpiar el campo de entrada después de enviar
+        this.question = '';
+
+        console.log('Entidades procesadas:', entities);
+      } catch (error) {
+        console.error('Error en la comunicación con Wit.ai:', error);
+        const errorMessage = {
+          text: 'Hubo un error al obtener la respuesta.',
+          sender: 'bot',
+          time: this.getTime()
+        };
+        this.messages.push(errorMessage);
       }
-
-      console.log('Entidades procesadas:', entities);
-    } catch (error) {
-      console.error('Error en la comunicación con Wit.ai:', error);
-      this.response = 'Hubo un error al obtener la respuesta.';
     }
+  }
+
+  // Método para obtener la hora actual
+  getTime(): string {
+    const date = new Date();
+    return `${date.getHours()}:${('0' + date.getMinutes()).slice(-2)} - ${date.toLocaleDateString()}`;
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,10 @@ module.exports = {
         "gray": "#2D3748",
         "opaque": "#1A202C",
         "red": "#FF0000",
+        "dark-100": "#3A3541",
+        "gray-200": "#89868D",
+        "gray-300": "#B4B2B7",
+        
       },
       customBlue: {
         "blue-light": "#63B3ED",
@@ -27,7 +31,21 @@ module.exports = {
       },
     },
     fontFamily: {
-      // Tipografía 
+      sans: ["Lato", "sans-serif"],
+    },
+    fontSize: {
+      "display-large": ["32px", { fontWeight: "900" }],
+      "display-medium": ["30px", { fontWeight: "700" }],
+      "display-small": ["28px", { fontWeight: "700" }],
+      "heading-large": ["26px", { fontWeight: "900" }],
+      "heading-medium": ["24px", { fontWeight: "700" }],
+      "heading-small": ["22px", { fontWeight: "700" }],
+      "title-large": ["20px", { fontWeight: "500" }],
+      "title-medium": ["18px", { fontWeight: "500" }],
+      "title-small": ["16px", { fontWeight: "500" }],
+      "body-large": ["16px", { fontWeight: "400" }],
+      "body-medium": ["14px", { fontWeight: "400" }],
+      "body-small": ["12px", { fontWeight: "400" }],
     },
     extend: {
       colors: {
